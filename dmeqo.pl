86b027173f0d5ee535f6df553c3f5700fe6d394e
Leandro Santiago <leandrosansilva@gmail.com>
2016-01-05 23:36:41 +0100

    Some more predicates... I'm getting there...

--- dmeqo.pl
+++ dmeqo.pl
@@ -1,14 +1,49 @@
 % Vim, this is Prolog!
 
 empty_field([H, W], field([], Empty)) :-
-  MaxX is W - 1,
-  MaxY is H - 1,
-  findall(X, between(0, MaxX, X), Xs),
-  findall(Y, between(0, MaxY, Y), Ys), 
-  findall(pos(XX, YY), (member(XX, Xs), member(YY, Ys)), GenEmpty),
+  succ(MaxX, W),
+  succ(MaxY, H),
+  findall(
+    pos(X, Y), (
+      between(0, MaxX, X), 
+      between(0, MaxY, Y)),
+    GenEmpty),
   list_to_ord_set(GenEmpty, Empty).
 
-add_tile(field(Used, Empty), [X, Y], field(NewUsed, NewEmpty)) :-
-  tile(X, Y, _) = Tile,
-  ord_add_element(Used, Tile, NewUsed),
+% it's private because in the real game
+% only tile with value 2 can be added
+add_tile_private(field(Used, Empty), [X, Y, TileValue], field(NewUsed, NewEmpty)) :-
+  ord_add_element(Used, tile(X, Y, TileValue), NewUsed),
   ord_del_element(Empty, pos(X, Y), NewEmpty).
+
+add_tile(Field, [X, Y], NewField) :- 
+  add_tile_private(Field, [X, Y, 2], NewField).
+
+add_tile_private(field(Used, Empty), [X, Y, TileValue], field(NewUsed, NewEmpty)) :-
+  ord_add_element(Used, tile(X, Y, TileValue), NewUsed),
+  ord_del_element(Empty, pos(X, Y), NewEmpty).
+ 
+
+next_tile_value(Value, NextValue) :-
+  NextValue is Value * 2.
+
+merge_tiles(tile(X1, Y1, V), tile(X2, Y2, V), Dir, tile(X3, Y3, NewValue)) :-
+  % FIXME: I still can refactor this code to remove copy-paste...
+  next_tile_value(V, NewValue),
+  (
+    Dir = left,
+    Y1 = Y2,
+    max_list([X1, X2], X3);
+
+    Dir = right,
+    Y1 = Y2,
+    min_list([X1, X2], X3);
+
+    Dir = up,
+    X1 = X2,
+    min_list([Y1, Y2], Y3);
+
+    Dir = down,
+    X1 = X2,
+    max_list([Y1, Y2], Y3)
+  ), !.
--- tests.pl
+++ tests.pl
@@ -18,7 +18,53 @@ test('create empty fields 4,5') :-
 
 test('create empty fields 4,5 and adds one tile') :-
   empty_field([4, 5], Field),
-  add_tile(Field, [0, 0], field([tile(0, 0, _)], EmptyPositions)),
+  add_tile(Field, [0, 0], field([tile(0, 0, 2)], EmptyPositions)),
   length(EmptyPositions, 19).
-  
+
+test('tile evolution') :-
+  next_tile_value(2, 4),
+  next_tile_value(4, 8),
+  next_tile_value(8, 16),
+  next_tile_value(16, 32).
+
+% test('slide field with a single element to the right') :-
+%   empty_field([4, 5], Field),
+%   add_tile(Field, [1, 2], NewField),
+%   move_tiles(NewField, left, field([tile(4, 2)], Empty)),
+%   length(Empty, 19).
+
+test('merge two tiles') :-
+  merge_tiles(tile(1, 2, 2), tile(3, 2, 2), left, tile(3, 2, 4)),
+  merge_tiles(tile(3, 2, 2), tile(1, 2, 2), left, tile(3, 2, 4)),
+
+  merge_tiles(tile(1, 2, 2), tile(3, 2, 2), right, tile(1, 2, 4)),
+  merge_tiles(tile(3, 2, 2), tile(1, 2, 2), right, tile(1, 2, 4)),
+
+  merge_tiles(tile(3, 1, 4), tile(3, 2, 4), up, tile(3, 1, 8)),
+  merge_tiles(tile(3, 2, 4), tile(3, 1, 4), up, tile(3, 1, 8)),
+
+  merge_tiles(tile(3, 2, 4), tile(3, 1, 4), down, tile(3, 2, 8)),
+  merge_tiles(tile(3, 2, 4), tile(3, 1, 4), down, tile(3, 2, 8)),
+
+  % cannot merge non-related tiles (diagonal)
+  not(merge_tiles(tile(3, 2, 4), tile(2, 1, 4), down, Merged)),
+  not(merge_tiles(tile(3, 2, 4), tile(2, 1, 4), up, Merged)),
+  not(merge_tiles(tile(3, 2, 4), tile(2, 1, 4), left, Merged)),
+  not(merge_tiles(tile(3, 2, 4), tile(2, 1, 4), right, Merged)),
+
+  % cannot merge tiles with different values
+  not(merge_tiles(tile(1, 2, 2), tile(3, 2, 8), left, Merged)),
+  not(merge_tiles(tile(1, 2, 4), tile(3, 2, 2), right, Merged)),
+  not(merge_tiles(tile(3, 1, 16), tile(3, 2, 2), up, Merged)),
+  not(merge_tiles(tile(3, 2, 1024), tile(3, 1, 256), down, Merged)).
+
+test('Fill field with several tiles (dev helper)') :-
+  Positions = [[0, 0], [1,0], [0, 1]],
+  length(Positions, LengthPositions),
+  Value = 2,
+  empty_field([3, 7], Field),
+  add_tiles(Field, Value, Positions, field(UsedPositions, EmptyPositions)),
+  length(UsedPositions, LengthPositions),
+  length(EmptyPositions, 18).
+
 :- end_tests(dmeqo_tests).
